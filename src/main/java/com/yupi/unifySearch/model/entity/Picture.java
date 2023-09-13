636a790b1d518c28d6e12c935c0e53bdc00c086a
Bennie61 <bennylu98@163.com>
2023-09-13 20:28:27 +0800

    使用Jsoup库获取到HTML文档，然后从中解析出图片的标题和url地址，并封装成Picture类。

---
+++ src/main/java/com/yupi/unifySearch/model/entity/Picture.java
@@ -0,0 +1,14 @@
+package com.yupi.unifySearch.model.entity;
+
+import lombok.Data;
+
+/**
+ * @Description 图片
+ * @Author benny
+ **/
+
+@Data
+public class Picture {
+    private String title;
+    private String url;
+}
--- src/test/java/com/yupi/unifySearch/CrawlerTest.java
+++ src/test/java/com/yupi/unifySearch/CrawlerTest.java
@@ -5,13 +5,19 @@ import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.yupi.unifySearch.model.entity.Picture;
 import com.yupi.unifySearch.model.entity.Post;
 import com.yupi.unifySearch.service.PostService;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -56,4 +62,28 @@ public class CrawlerTest {
         boolean b = postService.saveBatch(postList);
         Assertions.assertTrue(b);
     }
+    @Test
+    void testFetchPicture() throws IOException {
+        int current = 1;
+        String url = "https://www.bing.com/images/search?q=头像&first=" + current;
+        Document doc = Jsoup.connect(url).get();
+        Elements elements = doc.select(".iuscp.isv");
+        List<Picture> pictures= new ArrayList<>();
+        for (Element element : elements) {
+            // 取图片地址 (murl)
+            String m = element.select(".iusc").get(0).attr("m");
+            Map<String, Object> map = JSONUtil.toBean(m, Map.class);
+            String murl = (String) map.get("murl");
+//            System.out.println(murl);
+            // 取标题
+            String title = element.select(".inflnk").get(0).attr("aria-label");
+//            System.out.println(title);
+            Picture picture = new Picture();
+            picture.setTitle(title);
+            picture.setUrl(murl);
+            pictures.add(picture);
+        }
+        System.out.println(pictures);
+    }
+
 }
