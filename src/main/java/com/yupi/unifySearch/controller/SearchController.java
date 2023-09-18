de93f1d90a46b34a133779949b24adb5942d0647
Bennie61 <bennylu98@163.com>
2023-09-18 17:21:12 +0800

    采用并发方式请求各类型的数据

--- src/main/java/com/yupi/unifySearch/controller/SearchController.java
+++ src/main/java/com/yupi/unifySearch/controller/SearchController.java
@@ -2,7 +2,9 @@ package com.yupi.unifySearch.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yupi.unifySearch.common.BaseResponse;
+import com.yupi.unifySearch.common.ErrorCode;
 import com.yupi.unifySearch.common.ResultUtils;
+import com.yupi.unifySearch.exception.BusinessException;
 import com.yupi.unifySearch.model.dto.post.PostQueryRequest;
 import com.yupi.unifySearch.model.dto.search.SearchRequest;
 import com.yupi.unifySearch.model.dto.user.UserQueryRequest;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 
 /**
@@ -39,17 +42,49 @@ public class SearchController {
     @PostMapping("/all")
     public BaseResponse<SearchVO> searchAll(@RequestBody SearchRequest searchRequest, HttpServletRequest request){
         String searchText = searchRequest.getSearchText();
-        Page<Picture> picturePage = pictureService.searchPicture(searchText, 1, 10);
-        UserQueryRequest userQueryRequest = new UserQueryRequest();
-        userQueryRequest.setUserName(searchText);
-        Page<UserVO> userVOPage = userService.listUserVOByPage(userQueryRequest);
-        PostQueryRequest postQueryRequest = new PostQueryRequest();
-        postQueryRequest.setSearchText(searchText);
-        Page<PostVO> postVOPage = postService.listPostVOByPage(postQueryRequest, request);
-        SearchVO searchVO = new SearchVO();
-        searchVO.setPictureList(picturePage.getRecords());
-        searchVO.setUserList(userVOPage.getRecords());
-        searchVO.setPostList(postVOPage.getRecords());
-        return ResultUtils.success(searchVO);
+
+//        Page<Picture> picturePage = pictureService.searchPicture(searchText, 1, 10);
+//        UserQueryRequest userQueryRequest = new UserQueryRequest();
+//        userQueryRequest.setUserName(searchText);
+//        Page<UserVO> userVOPage = userService.listUserVOByPage(userQueryRequest);
+//        PostQueryRequest postQueryRequest = new PostQueryRequest();
+//        postQueryRequest.setSearchText(searchText);
+//        Page<PostVO> postVOPage = postService.listPostVOByPage(postQueryRequest, request);
+
+        CompletableFuture<Page<UserVO>> userTask = CompletableFuture.supplyAsync(() -> {
+            UserQueryRequest userQueryRequest = new UserQueryRequest();
+            userQueryRequest.setUserName(searchText);
+            Page<UserVO> userVOPage = userService.listUserVOByPage(userQueryRequest);
+            return userVOPage;
+        });
+
+        CompletableFuture<Page<PostVO>> postTask = CompletableFuture.supplyAsync(() -> {
+            PostQueryRequest postQueryRequest = new PostQueryRequest();
+            postQueryRequest.setSearchText(searchText);
+            Page<PostVO> postVOPage = postService.listPostVOByPage(postQueryRequest, request);
+            return postVOPage;
+        });
+
+        CompletableFuture<Page<Picture>> pictureTask = CompletableFuture.supplyAsync(() -> {
+            Page<Picture> picturePage = pictureService.searchPicture(searchText, 1, 10);
+            return picturePage;
+        });
+
+        CompletableFuture.allOf(userTask, postTask, pictureTask).join();
+
+        try {
+            Page<UserVO> userVOPage = userTask.get();
+            Page<PostVO> postVOPage = postTask.get();
+            Page<Picture> picturePage = pictureTask.get();
+
+            SearchVO searchVO = new SearchVO();
+            searchVO.setUserList(userVOPage.getRecords());
+            searchVO.setPostList(postVOPage.getRecords());
+            searchVO.setPictureList(picturePage.getRecords());
+            return ResultUtils.success(searchVO);
+        } catch (Exception e) {
+            log.error("查询异常", e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "查询异常");
+        }
     }
 }
