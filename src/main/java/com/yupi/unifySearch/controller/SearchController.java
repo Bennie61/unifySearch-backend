35fc157a3bfb2b3d233a3b250f47c21e20feb7f6
Bennie61 <bennylu98@163.com>
2023-09-18 22:58:23 +0800

    添加前端请求参数type，后端根据type调用不同的service方法来查询不同类型的数据，让前端既可以一次搜出所有数据，又能够分别获取某一类数据（比如分页场景）。

--- src/main/java/com/yupi/unifySearch/controller/SearchController.java
+++ src/main/java/com/yupi/unifySearch/controller/SearchController.java
@@ -5,10 +5,12 @@ import com.yupi.unifySearch.common.BaseResponse;
 import com.yupi.unifySearch.common.ErrorCode;
 import com.yupi.unifySearch.common.ResultUtils;
 import com.yupi.unifySearch.exception.BusinessException;
+import com.yupi.unifySearch.exception.ThrowUtils;
 import com.yupi.unifySearch.model.dto.post.PostQueryRequest;
 import com.yupi.unifySearch.model.dto.search.SearchRequest;
 import com.yupi.unifySearch.model.dto.user.UserQueryRequest;
 import com.yupi.unifySearch.model.entity.Picture;
+import com.yupi.unifySearch.model.enums.SearchTypeEnum;
 import com.yupi.unifySearch.model.vo.PostVO;
 import com.yupi.unifySearch.model.vo.SearchVO;
 import com.yupi.unifySearch.model.vo.UserVO;
@@ -24,6 +26,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import org.apache.commons.lang3.StringUtils;
 
 
 /**
@@ -41,50 +44,69 @@ public class SearchController {
     private PictureService pictureService;
     @PostMapping("/all")
     public BaseResponse<SearchVO> searchAll(@RequestBody SearchRequest searchRequest, HttpServletRequest request){
+        String type = searchRequest.getType();
+        SearchTypeEnum searchTypeEnum = SearchTypeEnum.getEnumByValue(type);
+        ThrowUtils.throwIf(StringUtils.isBlank(type), ErrorCode.PARAMS_ERROR);
         String searchText = searchRequest.getSearchText();
+        // 搜索出所有数据
+        if (searchTypeEnum == null){
+            CompletableFuture<Page<UserVO>> userTask = CompletableFuture.supplyAsync(() -> {
+                UserQueryRequest userQueryRequest = new UserQueryRequest();
+                userQueryRequest.setUserName(searchText);
+                Page<UserVO> userVOPage = userService.listUserVOByPage(userQueryRequest);
+                return userVOPage;
+            });
 
-//        Page<Picture> picturePage = pictureService.searchPicture(searchText, 1, 10);
-//        UserQueryRequest userQueryRequest = new UserQueryRequest();
-//        userQueryRequest.setUserName(searchText);
-//        Page<UserVO> userVOPage = userService.listUserVOByPage(userQueryRequest);
-//        PostQueryRequest postQueryRequest = new PostQueryRequest();
-//        postQueryRequest.setSearchText(searchText);
-//        Page<PostVO> postVOPage = postService.listPostVOByPage(postQueryRequest, request);
+            CompletableFuture<Page<PostVO>> postTask = CompletableFuture.supplyAsync(() -> {
+                PostQueryRequest postQueryRequest = new PostQueryRequest();
+                postQueryRequest.setSearchText(searchText);
+                Page<PostVO> postVOPage = postService.listPostVOByPage(postQueryRequest, request);
+                return postVOPage;
+            });
 
-        CompletableFuture<Page<UserVO>> userTask = CompletableFuture.supplyAsync(() -> {
-            UserQueryRequest userQueryRequest = new UserQueryRequest();
-            userQueryRequest.setUserName(searchText);
-            Page<UserVO> userVOPage = userService.listUserVOByPage(userQueryRequest);
-            return userVOPage;
-        });
+            CompletableFuture<Page<Picture>> pictureTask = CompletableFuture.supplyAsync(() -> {
+                Page<Picture> picturePage = pictureService.searchPicture(searchText, 1, 10);
+                return picturePage;
+            });
 
-        CompletableFuture<Page<PostVO>> postTask = CompletableFuture.supplyAsync(() -> {
-            PostQueryRequest postQueryRequest = new PostQueryRequest();
-            postQueryRequest.setSearchText(searchText);
-            Page<PostVO> postVOPage = postService.listPostVOByPage(postQueryRequest, request);
-            return postVOPage;
-        });
+            CompletableFuture.allOf(userTask, postTask, pictureTask).join();
 
-        CompletableFuture<Page<Picture>> pictureTask = CompletableFuture.supplyAsync(() -> {
-            Page<Picture> picturePage = pictureService.searchPicture(searchText, 1, 10);
-            return picturePage;
-        });
+            try {
+                Page<UserVO> userVOPage = userTask.get();
+                Page<PostVO> postVOPage = postTask.get();
+                Page<Picture> picturePage = pictureTask.get();
 
-        CompletableFuture.allOf(userTask, postTask, pictureTask).join();
-
-        try {
-            Page<UserVO> userVOPage = userTask.get();
-            Page<PostVO> postVOPage = postTask.get();
-            Page<Picture> picturePage = pictureTask.get();
-
+                SearchVO searchVO = new SearchVO();
+                searchVO.setUserList(userVOPage.getRecords());
+                searchVO.setPostList(postVOPage.getRecords());
+                searchVO.setPictureList(picturePage.getRecords());
+                return ResultUtils.success(searchVO);
+            } catch (Exception e) {
+                log.error("查询异常", e);
+                throw new BusinessException(ErrorCode.SYSTEM_ERROR, "查询异常");
+            }
+        }else{
             SearchVO searchVO = new SearchVO();
-            searchVO.setUserList(userVOPage.getRecords());
-            searchVO.setPostList(postVOPage.getRecords());
-            searchVO.setPictureList(picturePage.getRecords());
+            switch (searchTypeEnum) {
+                case POST:
+                    PostQueryRequest postQueryRequest = new PostQueryRequest();
+                    postQueryRequest.setSearchText(searchText);
+                    Page<PostVO> postVOPage = postService.listPostVOByPage(postQueryRequest, request);
+                    searchVO.setPostList(postVOPage.getRecords());
+                    break;
+                case USER:
+                    UserQueryRequest userQueryRequest = new UserQueryRequest();
+                    userQueryRequest.setUserName(searchText);
+                    Page<UserVO> userVOPage = userService.listUserVOByPage(userQueryRequest);
+                    searchVO.setUserList(userVOPage.getRecords());
+                    break;
+                case PICTURE:
+                    Page<Picture> picturePage = pictureService.searchPicture(searchText, 1, 10);
+                    searchVO.setPictureList(picturePage.getRecords());
+                    break;
+                default:
+            }
             return ResultUtils.success(searchVO);
-        } catch (Exception e) {
-            log.error("查询异常", e);
-            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "查询异常");
         }
     }
 }
--- src/main/java/com/yupi/unifySearch/model/dto/search/SearchRequest.java
+++ src/main/java/com/yupi/unifySearch/model/dto/search/SearchRequest.java
@@ -2,19 +2,21 @@ package com.yupi.unifySearch.model.dto.search;
 
 import com.yupi.unifySearch.common.PageRequest;
 import lombok.Data;
-
+import lombok.EqualsAndHashCode;
 import java.io.Serializable;
 
 /**
  * @Description ToDo
  * @Author benny
  **/
-
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class SearchRequest extends PageRequest implements Serializable {
     /**
      * 搜索词
      */
     private String searchText;
+
+    private String type;
     private static final long serialVersionUID = 1L;
 }
