4c31f9c4d2b346d28dd994f471c74418b96ba1b7
Bennie61 <bennylu98@163.com>
2023-09-19 13:31:35 +0800

    学习了SpringBoot集成Knife4j的使用方法，将Knife4j作为项目的Swagger接口文档和接口测试工具。

--- src/main/java/com/yupi/unifySearch/config/Knife4jConfig.java
+++ src/main/java/com/yupi/unifySearch/config/Knife4jConfig.java
@@ -14,8 +14,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * Knife4j 接口文档配置
  * https://doc.xiaominfo.com/knife4j/documentation/get_start.html
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
  */
 @Configuration
 @EnableSwagger2
@@ -31,7 +29,7 @@ public class Knife4jConfig {
                         .version("1.0")
                         .build())
                 .select()
-                // 指定 Controller 扫描包路径
+                // 下面的代码用于告诉Swagger需要扫描的 Controller的包路径。
                 .apis(RequestHandlerSelectors.basePackage("com.yupi.unifySearch.controller"))
                 .paths(PathSelectors.any())
                 .build();
