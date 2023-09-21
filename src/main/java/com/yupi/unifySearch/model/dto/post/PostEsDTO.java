7737ddea698878c25010e005188ce5c044ae02cb
Bennie61 <bennylu98@163.com>
2023-09-22 00:04:58 +0800

    使用Spring Data Elasticsearch，通过Java来操作Elasticsearch；在pom文件配置了“spring-boot-starter-data-elasticsearch”依赖；编写了用于接收ES搜索结果的实体类PostEsDTO。

--- src/main/java/com/yupi/unifySearch/model/dto/post/PostEsDTO.java
+++ src/main/java/com/yupi/unifySearch/model/dto/post/PostEsDTO.java
@@ -11,24 +11,21 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
 /**
  * 帖子 ES 包装类
- *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
  **/
-// todo 取消注释开启 ES（须先配置 ES）
-//@Document(indexName = "post")
+@Document(indexName = "post")
 @Data
 public class PostEsDTO implements Serializable {
 
     private static final String DATE_TIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
 
     /**
-     * id
+     * id 唯一标识
      */
     @Id
     private Long id;
@@ -47,16 +44,6 @@ public class PostEsDTO implements Serializable {
      * 标签列表
      */
     private List<String> tags;
-
-    /**
-     * 点赞数
-     */
-    private Integer thumbNum;
-
-    /**
-     * 收藏数
-     */
-    private Integer favourNum;
 
     /**
      * 创建用户 id
